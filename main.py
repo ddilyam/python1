292740eddd3be20c88415f48d6377e47be45e2b2
ddilyam <dilmd@icloud.com>
2022-09-25 17:59:49 +0600

    лаб1

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-# This is a sample Python script.
+# циклы
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# import math
+# num = int(input())
+# i=1
+# while i<=num:
+#     print(f' Number is : {i} and cube of the {i} is :{pow(i, 3)}')
+#     i=i+1
+################################################################
+# arr = [int(n) for n in input().split()]
+# for i in range(len(arr) - 1, -1, -1):
+#     print(arr[i])
+################################################################
+# import math
+# num = int(input())
+# f=1
+# for i in range(2, num+1):
+#     f=f*i
+# print(f)
+################################################################
+# import math
+# num = int(input())
+# sum=0
+# for i in range(1, num+1):
+#     sum=sum+i
+# print(sum)
+# ################################################################
+# x = input('x = ')
+# if x != x[::-1]:  #потому что идем от конца к началу
+#     print('False')
+# else:
+#     print('True')
+################################################################
+#вложенные циклы
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-print('tbrmb')
